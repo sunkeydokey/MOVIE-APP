9ea2b0fe40eedfc112e84ccdc3b9fff23b40bf0a
sunkeydokey <sunkeydokey@gmail.com>
2023-02-21 15:49:02 +0900

    header 컴포넌트 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Component } from './core/movieApp';
+import TheHeader from './components/TheHeader';
 
 export default class App extends Component {
   render() {
+    const header = new TheHeader().el;
     const routerView = document.createElement('router-view');
 
-    this.el.append(routerView);
+    this.el.append(header, routerView);
   }
 }
